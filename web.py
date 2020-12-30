95da45d50d06311289028a76a216b9b9fd8975c6
hejun <hejun1130@163.com>
2020-12-30 15:22:07 +0800

    提交

--- web.py
+++ web.py
@@ -9,11 +9,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/index")
 def index():
     return "Hello World"
 
 
+@app.route("/login")
 def login():
     return "登录成功"
 
